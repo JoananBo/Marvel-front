ec662efc6d22bedf763862533dec233f9294f004
Joanan Boureille <joananboureille@Ordinateur-portable-de-Joanan.local>
2023-12-22 11:11:53 +0100

    pagination comics

--- src/routes/Comics.jsx
+++ src/routes/Comics.jsx
@@ -6,6 +6,8 @@ import Pagination from "../components/Pagination";
 const Comics = ({ search }) => {
   const [data, SetData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPage] = useState(18);
   // const imgNot = data.thumbnail.path.indexOf("image_not_available");
   const addEllipsis = (text, maxLength) => {
     return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
@@ -18,7 +20,7 @@ const Comics = ({ search }) => {
           `https://site--marvel-backend--zg6fw4jztfcn.code.run/comics?title=${search}`
         );
         console.log("response =>", response.data);
-        SetData(response.data);
+        SetData(response.data.results);
 
         setIsLoading(false);
       } catch (error) {
@@ -27,39 +29,60 @@ const Comics = ({ search }) => {
     };
     fetchData();
   }, [search]);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPost = data.slice(firstPostIndex, lastPostIndex);
+
   // const imgNot = data.thumbnail.path.indexOf("image_not_available");
   return isLoading ? (
     <p>Un peu de patience...</p>
   ) : (
-    <div className="comics-container">
-      {data.results.map((comics) => {
-        // console.log(comics);
-        // const imgNot = comics.thumbnail.path.indexOf("image_not_available");
-        return (
-          <Link
-            to={`/comic/${comics._id}`}
-            target="_blank"
-            style={{ textDecoration: "none" }}
-          >
-            <div className="comic-cards">
-              <div className="id-top">
-                <h2>{comics.title}</h2>
-              </div>
-              <div className="id-bot">
-                <img
-                  src={comics.thumbnail.path + "." + comics.thumbnail.extension}
-                  alt={comics.title}
-                />
+    <div>
+      <Pagination
+        totalPosts={data.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
+      <div className="comics-container">
+        {currentPost.map((comics) => {
+          // console.log(comics);
+          // const imgNot = comics.thumbnail.path.indexOf("image_not_available");
+          return (
+            <Link
+              to={`/comic/${comics._id}`}
+              target="_blank"
+              style={{ textDecoration: "none" }}
+            >
+              <div className="comic-cards">
+                <div className="id-top">
+                  <h2>{comics.title}</h2>
+                </div>
+                <div className="id-bot">
+                  <img
+                    src={
+                      comics.thumbnail.path + "." + comics.thumbnail.extension
+                    }
+                    alt={comics.title}
+                  />
 
-                <div>
-                  {/* <p>{comics.description}</p> */}
-                  {/* <p>{addEllipsis(comics.description, 1)}</p> */}
+                  <div>
+                    {/* <p>{comics.description}</p> */}
+                    {/* <p>{addEllipsis(comics.description, 1)}</p> */}
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
+      <Pagination
+        totalPosts={data.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
