88417be7f9f65006707542df0f7d290269df1d86
Joanan Boureille <joananboureille@Ordinateur-portable-de-Joanan.local>
2024-02-12 18:48:06 +0100

    footer +

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-box">
-        <p>Réalisé par Joanan Boureille</p>
+        <p>Projet réalisé par Joanan Boureille (2023/204)</p>
+        <p>Entraînement pour mon projet professionnel</p>
       </div>
     </footer>
   );
